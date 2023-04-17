71f3c0b097b709b9237ba716567f041917fd664c
Abhiram-500apps <abhiram.500apps@gmail.com>
2023-04-17 11:03:49 +0530

    amazon jobs

--- sample/sample/spiders/amazon.py
+++ sample/sample/spiders/amazon.py
@@ -3,18 +3,19 @@ import scrapy
 
 class AmazonJobsSpider(scrapy.Spider):
     name = 'amazon_jobs'
-    start_urls = ['https://www.amazon.jobs/en/jobs/2348059/business-intelligence-engineer-datacenter-security']
+    start_urls = ['https://www.amazon.jobs/en/search?base_query=Information+Systems+Engineer']
 
     def parse(self, response):
-        job_links_data=response.xpath("//div[@class='job-tile-lists col-12 mt-3 mt-md-0']/div/a").xpath('@href').getall()
-        print(job_links_data)
+        job_data=response.xpath("//div[@class='job-tile-lists col-12 mt-3 mt-md-0']/div/a").xpath('@href').getall()
+        # print(response.text)
+        print(job_data)
 
-        for job_link in job_links_data:
+        for job_link in job_data:
             yield scrapy.Request(url=job_link, callback=self.parse_job)
             
     def parse_job(self, response):
         print(response.text)
         job_dict = {}
-        title = response.xpath("//div[@class='styles__TitleOpeningContainer-sc-15yd6lj-13 vQfBh']/h1/text()").get()
+        title = response.xpath("//div[@class='job']/div/div/h3/text()").get()
         job_dict['title'] = title
         yield job_dict
